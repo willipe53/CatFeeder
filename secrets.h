c5f7dfa0c43b5e5584913fe25c0815cb8340316b
willipe53 <willipe53@gmail.com>
2022-03-14 11:21:59 -0400

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -3,27 +3,27 @@
 #define SECRET
 #define THINGNAME "CatFeeder"
 
-const char WIFI_SSID[] = "Williams";
-const char WIFI_PASSWORD[] = "teletubby443";
-const char AWS_IOT_ENDPOINT[] = "a2df2s49pb3if0-ats.iot.us-east-1.amazonaws.com";
+const char WIFI_SSID[] = "SOMETHING";
+const char WIFI_PASSWORD[] = "SOMETHING";
+const char AWS_IOT_ENDPOINT[] = "SOMETHING.iot.us-east-1.amazonaws.com";
 
 // Amazon Root CA 1
 static const char AWS_CERT_CA[] PROGMEM = R"EOF(
 -----BEGIN CERTIFICATE-----
-
+SOMETHING
 -----END CERTIFICATE-----
 )EOF";
 
 // Device Certificate
 static const char AWS_CERT_CRT[] PROGMEM = R"KEY(
 -----BEGIN CERTIFICATE-----
-
+SOMETHING
 -----END CERTIFICATE-----
 )KEY";
 
 // Device Private Key
 static const char AWS_CERT_PRIVATE[] PROGMEM = R"KEY(
 -----BEGIN RSA PRIVATE KEY-----
-
+SOMETHING
 -----END RSA PRIVATE KEY-----
 )KEY";
